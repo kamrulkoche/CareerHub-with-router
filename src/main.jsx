93b841464181dea0d1a0ba77f6d90625da18900f
kamrulkoche <kamrulkoche@gmail.com>
2024-07-08 03:33:08 +0600

    CareerHub-with-router update

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
-    errorElement:<ErrorPage></ErrorPage>,
+    errorElement: <ErrorPage></ErrorPage>,
 
     children: [
       {
