3710eb213b49742a2348afbf251e3148ea20db0f
kamrulkoche <kamrulkoche@gmail.com>
2024-07-08 14:30:01 +0600

    update

--- src/components/Job/Job.jsx
+++ src/components/Job/Job.jsx
@@ -1,3 +1,5 @@
+import { MdLocationOn } from "react-icons/md";
+import { HiOutlineCurrencyDollar } from "react-icons/hi2";
 const Job = ({ job }) => {
   const {
     logo,
@@ -17,8 +19,22 @@ const Job = ({ job }) => {
         <h2 className="card-title">{job_title}</h2>
         <p>{company_name}</p>
         <div>
-          <button className="px-6 py-2 font-semibold border rounded border-[#7E90FE] mr-4 text-[#7E90FE]">{remote_or_onsite}</button>
-          <button className="px-6 py-2 font-semibold border rounded border-[#7E90FE] mr-4 text-[#7E90FE]">{job_type}</button>
+          <button className="px-6 py-2 font-semibold border rounded border-[#7E90FE] mr-4 text-[#7E90FE]">
+            {remote_or_onsite}
+          </button>
+          <button className="px-6 py-2 font-semibold border rounded border-[#7E90FE] mr-4 text-[#7E90FE]">
+            {job_type}
+          </button>
+        </div>
+        <div className="my-2 flex">
+          <h2 className="flex items-center">
+            <MdLocationOn className="text-2xl mr-2"></MdLocationOn>
+            {location}
+          </h2>
+          <h2 className="flex items-center ml-4">
+            <HiOutlineCurrencyDollar className="text-2xl mr-2"></HiOutlineCurrencyDollar>
+            {salary}
+          </h2>
         </div>
         <div className="card-actions">
           <button className="btn btn-primary">View Details</button>
